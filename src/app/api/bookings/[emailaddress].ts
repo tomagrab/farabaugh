22e8f661459690451b9a0293dfc1f2cdb3e95bf6
Tommy Grabowski <60988025+tomagrab@users.noreply.github.com>
2023-11-14 09:30:30 -0500

    Added api route for fetching bookings - does not work atm

--- src/app/api/bookings/[emailaddress].ts
+++ src/app/api/bookings/[emailaddress].ts
@@ -5,29 +5,35 @@ type ErrorResponse = {
   message: string;
 };
 
-type Booking = {
-  // Define the type structure for your booking, e.g.,
+type BookingResponse = {
+  // Adjust this type according to the data you want to send back
   id: number;
-  userId: number;
   serviceDate: Date;
   status: string;
   notes: string | null;
-  // Add other fields as needed
+  // Include any other relevant fields
 };
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Booking[] | ErrorResponse>
+  res: NextApiResponse<BookingResponse[] | ErrorResponse>
 ) {
   if (req.method === "GET") {
-    const emailAddress = req.query.emailAddress as string; // Casting as string
-    console.log("emailAddress", emailAddress);
+    const emailAddress = req.query.emailAddress as string;
 
     try {
-      const bookings = await db.booking.findMany({
-        where: { user: { email: emailAddress } },
+      const user = await db.user.findUnique({
+        where: { email: emailAddress },
+        include: {
+          bookings: true, // Include the user's bookings
+        },
       });
-      res.status(200).json(bookings);
+
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      res.status(200).json(user.bookings);
     } catch (error) {
       res.status(500).json({ message: "Error fetching bookings" });
     }
